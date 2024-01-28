c419034d4ab9fc84940a4e8475f6ad5eff5e7836
Howarth <celagiadev@gmail.com>
2024-01-28 14:08:18 +0000

    fixed app.css emptying on build

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,7 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-import './bootstrap';
+import './bootstrap.js';
 import { createApp } from 'vue';
 
 /**
@@ -15,8 +15,8 @@ import { createApp } from 'vue';
 
 const app = createApp({});
 
-import ExampleComponent from './components/ExampleComponent.vue';
-app.component('example-component', ExampleComponent);
+//import ExampleComponent from './components/ExampleComponent.vue';
+//app.component('example-component', ExampleComponent);
 
 /**
  * The following block of code may be used to automatically register your
