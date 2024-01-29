3d7427a70c60757975dab263b34fe1b6feb52d8d
Howarth <celagiadev@gmail.com>
2024-01-29 15:43:26 +0000

    .

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,10 @@
             <img src="https://avatars.githubusercontent.com/u/36736971?v=4" style="height: 175px" class="rounded-circle">
         </div>
         <div class="col-9 pt-5">
-            <div><h1>{{ $user->username }}</h1></div>
+            <div class="d-flex justify-content-between align-items-baseline">
+                <h1>{{ $user->username }}</h1>
+                <a href='#'>Add New Post</a>
+            </div>
             <div class="d-flex">
                 <div class="pr-5"><strong>3</strong> posts</div>
                 <div class="pr-5"><strong>158K</strong> followers</div>
