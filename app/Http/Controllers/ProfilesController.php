0eef421dd8966c5f06c64d040742c83afc7a42f7
Howarth <celagiadev@gmail.com>
2024-02-02 10:36:14 +0000

    added some profiles

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -53,7 +53,7 @@ class ProfilesController extends Controller
         $data = request()->validate([
             'title' => 'required',
             'description' => 'required',
-            'url' => 'url',
+            'url' => '' ?? 'url',
             'image' => '',
         ]);
 
